aa812328fa89cb71e9f149407427e259defcf736
mgiridhar <mgiridhar11@gmail.com>
2018-05-03 22:23:15 -0700

    adding more tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,6 +5,11 @@ import Timer from '../Timer';
 import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 
+window.HTMLMediaElement.prototype.load = () => { /* do nothing */ };
+window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
+window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
+window.HTMLMediaElement.prototype.addTextTrack = () => { /* do nothing */ };
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -50,27 +55,153 @@ it('focusMins selection change and set/reset timer button click', () => {
 
 it('clicking start should call timerStart', () => {
   const spy = sinon.spy(App.prototype, 'timerStart');
+  const spyChild = sinon.spy(Timer.prototype, 'startTimer');
   const wrapper = mount(<App />);
   
   wrapper.find('#start').find('button').simulate('click');
   expect(spy.calledOnce).toBe(true);
+  expect(spyChild.calledOnce).toBe(true);
   wrapper.unmount();
 });
 
 it('clicking pause should call timerPause', () => {
   const spy = sinon.spy(App.prototype, 'timerPause');
+  const spyChild = sinon.spy(Timer.prototype, 'pauseTimer');
   const wrapper = mount(<App />);
   
   wrapper.find('#pause').find('button').simulate('click');
   expect(spy.calledOnce).toBe(true);
+  expect(spyChild.calledOnce).toBe(true);
   wrapper.unmount();
 });
 
 it('clicking skip should call timerSkip', () => {
   const spy = sinon.spy(App.prototype, 'timerSkip');
+  const spyChild = sinon.spy(Timer.prototype, 'skipTimer');
   const wrapper = mount(<App />);
   
   wrapper.find('#skip').find('button').simulate('click');
   expect(spy.calledOnce).toBe(true);
+  expect(spyChild.calledOnce).toBe(true);
+  wrapper.unmount();
+});
+
+it('focus time to short break and back to focus with alerts', () => {
+  const spy = sinon.spy(App.prototype, 'toggleTimer');  
+  //const spyTick = sinon.spy(Timer.prototype, 'tick'); console.log(spyTick.callCount);
+  const wrapper = mount(<App />);
+  var clock = sinon.useFakeTimers();
+  const focusMins = parseInt(wrapper.find('#focusMins').instance().value);
+  const sbreakMins = parseInt(wrapper.find('#sbreakMins').instance().value);
+
+  //wrapper.find(Timer).instance().forceUpdate();
+  //wrapper.update(); //wrapper.find(Timer).prop('toggle')();
+  expect(wrapper.find(Timer).instance().props.title).toBe('Focus');
+  expect(wrapper.find('.timer').render().text()).toBe(focusMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(1);
+  wrapper.find('#start').find('button').simulate('click');
+  clock.tick((focusMins - 1) * 60 * 1000);
+  //console.log(wrapper.find(Timer).instance().state);
+  expect(spy.calledOnce).toBe(false);
+  clock.tick(1000);
+  expect(spy.calledOnce).toBe(true);
+  //console.log(wrapper.find(Timer).instance().state);
+
+  expect(wrapper.find(Timer).instance().props.title).toBe('Short Break');
+  expect(wrapper.find('.timer').render().text()).toBe('0' + sbreakMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(1);
+  clock.tick((sbreakMins - 1) * 60 * 1000);
+  expect(spy.calledTwice).toBe(false);
+  clock.tick(1000);
+  expect(spy.calledTwice).toBe(true);
+
+  expect(wrapper.find(Timer).instance().props.title).toBe('Focus');
+  expect(wrapper.find('.timer').render().text()).toBe(focusMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(2);
+  //console.log(wrapper.find(Timer).instance().state);
+
+  spy.restore();
+  clock.restore();
+  wrapper.unmount();
+});
+
+it('focus time to long break and back to focus with alerts', () => {
+  const spy = sinon.spy(App.prototype, 'toggleTimer');
+  const wrapper = mount(<App />);
+  var clock = sinon.useFakeTimers();
+  const focusMins = parseInt(wrapper.find('#focusMins').instance().value);
+  const sbreakMins = parseInt(wrapper.find('#sbreakMins').instance().value);
+  const lbreakMins = parseInt(wrapper.find('#lbreakMins').instance().value);
+  
+  wrapper.find('#start').find('button').simulate('click');
+  clock.tick((focusMins - 1) * 60 * 1000);
+  clock.tick(1000);
+  clock.tick((sbreakMins - 1) * 60 * 1000);
+  clock.tick(1000);
+  clock.tick((focusMins - 1) * 60 * 1000);
+  clock.tick(1000);
+  clock.tick((sbreakMins - 1) * 60 * 1000);
+  clock.tick(1000);
+  clock.tick((focusMins - 1) * 60 * 1000);
+  clock.tick(1000);
+  clock.tick((sbreakMins - 1) * 60 * 1000);
+  clock.tick(1000);
+
+  expect(wrapper.find(Timer).instance().props.title).toBe('Focus');
+  expect(wrapper.find('.timer').render().text()).toBe(focusMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(4);
+  clock.tick((focusMins - 1) * 60 * 1000);
+  expect(spy.callCount).toBe(6);
+  clock.tick(1000);
+  expect(spy.callCount).toBe(7);
+  expect(wrapper.find(Timer).instance().props.title).toBe('Long Break');
+  expect(wrapper.find('.timer').render().text()).toBe(lbreakMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(4);
+  clock.tick((lbreakMins - 1) * 60 * 1000);
+  expect(spy.callCount).toBe(7);
+  clock.tick(1000);
+  expect(spy.callCount).toBe(8);
+  expect(wrapper.find(Timer).instance().props.title).toBe('Focus');
+  expect(wrapper.find('.timer').render().text()).toBe(focusMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(5);
+
+  spy.restore();
+  clock.restore();
+  wrapper.unmount();
+});
+
+it('pomodoro ends after total rounds', () => {
+  const spy = sinon.spy(App.prototype, 'toggleTimer');
+  const wrapper = mount(<App />);
+  var clock = sinon.useFakeTimers();
+
+  const focusMins = parseInt(wrapper.find('#focusMins').instance().value);
+  const sbreakMins = parseInt(wrapper.find('#sbreakMins').instance().value);
+  const lbreakMins = parseInt(wrapper.find('#lbreakMins').instance().value);
+  const totalRounds = parseInt(wrapper.find('#totalRounds').instance().value);
+
+  wrapper.find('#start').find('button').simulate('click');
+  for(var i = 1; i <= totalRounds; i++) {
+    if(i % 4 === 0) {
+      clock.tick((focusMins - 1) * 60 * 1000);
+      clock.tick(1000);
+      clock.tick((lbreakMins - 1) * 60 * 1000);
+      clock.tick(1000);
+    }
+    else {
+      clock.tick((focusMins - 1) * 60 * 1000);
+      clock.tick(1000);
+      clock.tick((sbreakMins - 1) * 60 * 1000);
+      clock.tick(1000);
+    }
+  }
+
+  clock.tick(2000);
+  expect(wrapper.find(Timer).instance().props.title).toBe('Focus');
+  expect(wrapper.find('.timer').render().text()).toBe(focusMins + ':00');
+  expect(wrapper.find(Timer).instance().state.round).toBe(1);
+
+  spy.restore();
+  clock.restore();
   wrapper.unmount();
 });
--- src/__tests__/Timer.test.js
+++ src/__tests__/Timer.test.js
@@ -21,10 +21,12 @@ it('correct initialization for display', () => {
     expect(wrapper.state().rounds).toBe(rounds);
     expect(wrapper.state().timerState).toBe('ready');
     expect(wrapper.find('.timer').render().text()).toBe(mins+':00');
+
+    clock.restore();
     wrapper.unmount();
 });
 
-it('timer runs correctly - decrements every second', () => {
+it('timer runs correctly - decrements every second and unaffected by multiple start clicks', () => {
     var mins = '25', rounds = 12;
     const wrapper = mount( <Timer mins={mins} title='Focus' toggle={()=>{}} round={1} rounds={rounds} /> );
     var clock = sinon.useFakeTimers();
@@ -43,12 +45,22 @@ it('timer runs correctly - decrements every second', () => {
     expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-2) + ':26');
     //console.log(wrapper.find('.timer').render().text());
     
+    // additinal start clicks should modify the timer state or stop the timer
+    wrapper.instance().startTimer();
+    clock.tick(2000); // after 2 more seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-2));
+    expect(wrapper.state().secs).toBe('24');
+    expect(wrapper.state().timerState).toBe('running');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-2) + ':24');
+
+    clock.restore();
     wrapper.unmount();
 });
 
-it('timer stops at the end', () => {
+it('timer stops at the end and calls toggle', () => {
     var mins = '25', rounds = 12;
-    const wrapper = mount( <Timer mins={mins} title='Focus' toggle={()=>{}} round={1} rounds={rounds} /> );
+    var fakeToggle = sinon.fake();
+    const wrapper = mount( <Timer mins={mins} title='Focus' toggle={fakeToggle} round={1} rounds={rounds} /> );
     var clock = sinon.useFakeTimers();
     wrapper.instance().startTimer();
     clock.tick(parseInt(mins) * 60 * 1000);
@@ -60,12 +72,94 @@ it('timer stops at the end', () => {
     //console.log(wrapper.find('.timer').text());
     //console.log(wrapper.html());
 
-    //after 2 seconds is the timer still runnging?
+    // calls toggle
+    expect(fakeToggle.calledOnce).toBe(true);
+
+    //after 2 seconds is the timer still running?
     clock.tick(2000);
     expect(wrapper.state().mins).toBe('00');
     expect(wrapper.state().secs).toBe('00');
     expect(wrapper.state().timerState).toBe('stopped');
     expect(wrapper.find('.timer').render().text()).toBe('00:00');
 
+    clock.restore();
+    wrapper.unmount();
+});
+
+it('pausing timer and resume', () => {
+    var mins = '25', rounds = 12;
+    const wrapper = mount( <Timer mins={mins} title='Focus' toggle={()=>{}} round={1} rounds={rounds} /> );
+    var clock = sinon.useFakeTimers();
+    wrapper.instance().startTimer();
+    clock.tick(4000); // after 4 seconds
+
+    // testing pause
+    wrapper.instance().pauseTimer();
+    clock.tick(2000); // after 2 more seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-1));
+    expect(wrapper.state().secs).toBe('56');
+    expect(wrapper.state().timerState).toBe('paused');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-1) + ':56');
+
+    // one more pause should change the state of timer
+    wrapper.instance().pauseTimer();
+    clock.tick(2000); // after 2 more seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-1));
+    expect(wrapper.state().secs).toBe('56');
+    expect(wrapper.state().timerState).toBe('paused');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-1) + ':56');
+
+    // testing resume
+    wrapper.instance().startTimer();
+    clock.tick(3000); // after 3 seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-1));
+    expect(wrapper.state().secs).toBe('53');
+    expect(wrapper.state().timerState).toBe('running');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-1) + ':53');
+
+    clock.restore();
+    wrapper.unmount();
+});
+
+it('skipping timer', () => {
+    var mins = '25', rounds = 12;
+    const wrapper = mount( <Timer mins={mins} title='Focus' toggle={()=>{}} round={1} rounds={rounds} /> );
+    var clock = sinon.useFakeTimers();
+    wrapper.instance().startTimer();
+    clock.tick(3000); // after 3 seconds
+    wrapper.instance().skipTimer();
+    expect(wrapper.state().mins).toBe('00');
+    expect(wrapper.state().secs).toBe('00');
+    expect(wrapper.state().timerState).toBe('stopped');
+    expect(wrapper.find('.timer').render().text()).toBe('00:00');
+    clock.restore();
+    wrapper.unmount();
+});
+
+it('stopping timer and starting back', () => {
+    var mins = '25', rounds = 12;
+    const wrapper = mount( <Timer mins={mins} title='Focus' toggle={()=>{}} round={1} rounds={rounds} /> );
+    var clock = sinon.useFakeTimers();
+    wrapper.instance().startTimer();
+    clock.tick(4000); // after 4 seconds
+
+    // testing pause
+    wrapper.instance().stopTimer();
+    expect(wrapper.state().timerState).toBe('stopped');
+    clock.tick(2000); // after 2 more seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-1));
+    expect(wrapper.state().secs).toBe('56');
+    expect(wrapper.state().timerState).toBe('stopped');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-1) + ':56');
+
+    // now issueing a start should restart timer from beginning
+    wrapper.instance().startTimer();
+    clock.tick(2000); // after 2 seconds
+    expect(wrapper.state().mins).toBe('' + (parseInt(mins)-1));
+    expect(wrapper.state().secs).toBe('58');
+    expect(wrapper.state().timerState).toBe('running');
+    expect(wrapper.find('.timer').render().text()).toBe((parseInt(mins)-1) + ':58');
+    
+    clock.restore();
     wrapper.unmount();
 });
